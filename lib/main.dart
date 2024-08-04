210ddf6903da487107b0ecd29b85b0eadab86cdf
AlyssonTrugilio <alysson.trugilio.dev@gmail.com>
2024-08-04 00:43:42 -0300

    feat: implementado package device_preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:weather_app/core/app_widget.dart';
 
 void main() {
-  runApp(const AppWidget());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      tools: const [
+        ...DevicePreview.defaultTools,
+      ],
+      builder: (context) => const AppWidget(),
+    ),
+  );
 }
